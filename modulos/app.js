8f00ead1320baafee784590bab8f33f2d204720d
Marja <castanedamarjaQgmail.com>
2022-01-24 17:43:30 -0600

    ejercicios de modulos realizados en clase

---
+++ modulos/app.js
@@ -0,0 +1,10 @@
+import{ sumar, multuplicar, mostrarResultado} from './calculadora.js'
+
+let resultado = sumar (10 , 20)
+console.log (resultado);
+
+let resultadoMultiplicar = multuplicar (12, 46)
+
+mostrarResultado ('multiplicacion', 12 , 46 , resultadoMultiplicar)
+
+
